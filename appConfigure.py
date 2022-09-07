5775cb22f772ee95bac9c6bc3caa53647a7b34c2
razaahmad333 <raza82504@gmail.com>
2022-09-07 20:38:44 +0530

    changed api secret key

--- appConfigure.py
+++ appConfigure.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vegetables.db'
-app.secret_key = b'3ba197f32520c8e7daaab75581c20a0276a421553dc7c06f84bfca0d6bc86375'
+app.secret_key = b'generate api keys from where i dont know '
